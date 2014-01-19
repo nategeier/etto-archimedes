9a48783779b1478c3adfe624150d87ee7bdf090d
Nate <nate@interactivebalance.com>
2014-01-19 13:16:34 -0800

    from vegas edits

--- test/api/Tier.js
+++ test/api/Tier.js
@@ -1,19 +1,18 @@
-
-
-var app = require("../../index")
-  , assert = require("assert")
-  , request = require("supertest");
+var app = require("../../index"),
+  assert = require("assert"),
+  request = require("supertest");
 
 var route = {
   path: "/tier/",
   collection: "tiers"
 };
 
-var createAndTest = require("../helpers").createAndTestFrom(route.collection);
-var remove = require("../helpers").removeFrom(route.collection);
-
-describe("Tier", function() { 
+describe("Tier", function() {
   describe("POST " + route.path, function() {
+
+    var loadedTier = {
+      _id: '52d436dfcdf742900d220b7a'
+    }
 
     var new_tier = {
       title: 'Galaxy',
@@ -29,91 +28,90 @@ describe("Tier", function() {
 
     it("should add a tier", function(done) {
       request(app)
-      .post('/tier/add')
-      .send(new_tier)
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res){
-        assert.equal(res.body.title, "Galaxy");
-        done();
-      })  
+        .post('/tier/add')
+        .send(new_tier)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          assert.equal(res.body.title, "Galaxy");
+          done();
+        })
     });
 
 
 
     it("should add same tier and throw 500", function(done) {
       request(app)
-      .post('/tier/add')
-      .send(new_tier)
-      .expect('Content-Type', /json/)
-      .expect(500, done)
-      
+        .post("/tier/add")
+        .send(new_tier)
+        .expect('Content-Type', /json/)
+        .expect(500, done)
+
     });
 
 
 
     it("should add a child tier to student tier", function(done) {
-      
-      request(app)
-      .post('/tier/add')
-      .send(childTier)
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res){
 
-        assert.equal(res.body.title, 'Earth');
-        done();
-      })  
+      request(app)
+        .post('/tier/add')
+        .send(childTier)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+
+          assert.equal(res.body.title, 'Earth');
+          done();
+        })
     });
 
 
     it("should find a tier", function(done) {
       request(app)
-      .get('/tier/52cdd0dbf930a97e28363d52')
-      .send(new_tier)
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res){
-        assert.equal(res.body.title,  'Galaxy');
-        done();
-      })  
+        .get('/tier/52cdd0dbf930a97e28363d52')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          assert.equal(res.body.title, 'Galaxy');
+          done();
+        })
     });
 
 
     it("should list tiers children and count users", function(done) {
-      
+
       request(app)
-      .post('/tier/list_children_and_count_users')
-      .send(new_tier)
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res){
-        console.log(res.body)
-        assert.equal(err, null);
-        done();
-      })  
+        .post('/tier/list_children_and_count_users')
+        .send(loadedTier)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+
+          assert.equal(err, null);
+          done();
+        })
 
     });
 
     it("should remove a tier", function(done) {
-      
+
       request(app)
-      .post('/tier/remove').send(new_tier)
-      .expect('Content-Type', /json/)
-      .expect(200, done)
+        .post('/tier/remove').send(new_tier)
+        .expect('Content-Type', /json/)
+        .expect(200, done)
     });
 
 
     it("should remove child tier", function(done) {
-      
+
       request(app)
-      .post('/tier/remove').send(childTier)
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res){
-        assert.equal(err, null);
-        done();
-      })  
+        .post('/tier/remove').send(childTier)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          assert.equal(err, null);
+          done();
+        })
 
     });
 
