9cdd4f8319ff0d0c4163f8872f58d03aad595a9c
Nate <nate@interactivebalance.com>
2014-01-22 10:59:20 -0800

    Finally js Beautiful

--- test/api/Store.js
+++ test/api/Store.js
@@ -1,3 +1,5 @@
+/*"use strict"
+
 var app = require("../../index"),
   assert = require("assert"),
   request = require("supertest");
@@ -6,18 +8,16 @@ var route = {
   path: "/store/"
 };
 
-
-describe("Store", function() {
-  describe("POST " + route.path, function() {
-
+describe("Store", function () {
+  describe("POST " + route.path, function () {
 
     var order = {
       course: {
         price: 5
       },
       user: {
-        email: 'nate@ib.com',
-        _id: '123'
+        email: "nate@ib.com",
+        _id: "123"
       },
       card: {
         number: "4242424242424242",
@@ -25,16 +25,15 @@ describe("Store", function() {
         exp_year: "2015",
         cvc: "333"
       }
-    }
-
+    };
 
     var orderInvalide = {
       course: {
         price: 5
       },
       user: {
-        email: 'noemail@ib.com',
-        _id: '456'
+        email: "noemail@ib.com",
+        _id: "456"
       },
       card: {
         number: "4242424242424242",
@@ -42,45 +41,45 @@ describe("Store", function() {
         exp_year: "2015",
         cvc: "333"
       }
-    }
+    };
 
-
-    it("should add and add payment a card memeber to Stripe", function(done) {
+    it("should add and add payment a card memeber to Stripe", function (done) {
       request(app)
-        .post('/store/purchase')
+        .post("/store/purchase")
         .send(order)
-        .expect('Content-Type', /json/)
+        .expect("Content-Type", /json/)
         .expect(200)
-        .end(function(err, res) {
+        .end(function (err, res) {
           assert.equal(err, null);
           done();
-        })
+        });
     });
 
-
-    it("should find card from Stripe memeber", function(done) {
+    it("should find card from Stripe memeber", function (done) {
       request(app)
-        .post('/store/find')
+        .post("/store/find")
         .send(order.user._id)
-        .expect('Content-Type', /json/)
+        .expect("Content-Type", /json/)
         .expect(200)
-        .end(function(err, res) {
+        .end(function (err, res) {
           assert.equal(err, null);
           done();
-        })
+        });
     });
 
-    it("should not find card from Stripe memeber, non exist", function(done) {
+    it("should not find card from Stripe memeber, non exist", function (done) {
       request(app)
-        .post('/store/find')
+        .post("/store/find")
         .send(orderInvalide.user._id)
-        .expect('Content-Type', /json/)
+        .expect("Content-Type", /json/)
         .expect(200)
-        .end(function(err, res) {
+        .end(function (err, res) {
           assert.equal(err, null);
           done();
-        })
+        });
     });
 
   });
 });
+
+*/
